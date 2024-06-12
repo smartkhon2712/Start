f074709583c4d29f998cf60d2cf67b32a76dd338
smartkhon2712 <smartkhon2712@gmail.com>
2024-06-13 00:58:30 +0800

    Start project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      dark: "#212529",
+      "light-dark": "rgba(33,37,41,50%)",
+      green: "#74C69D",
+    },
+  },
+};
