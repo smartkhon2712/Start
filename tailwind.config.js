4e23533eb627fcfd29d8039dd3949f6e65f144d8
smartkhon2712 <smartkhon2712@gmail.com>
2024-06-15 14:46:17 +0800

    fixed again

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ tailwind.config = {
     colors: {
       black: "#000000",
       white: "#FFFFFF",
+      cream: "#EEEEEE",
       dark: "#212529",
       "light-dark": "rgba(33,37,41,50%)",
       green: "#74C69D",
